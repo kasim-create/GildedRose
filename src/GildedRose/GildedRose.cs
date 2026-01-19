5cabee9a006f0592848f98d1fab3b2773a1d2801
kasim <kasim@>
2026-01-19 19:46:03 +0000

    Modified conditions as days start from 0 index. FIxed broken unit tests. Added additional unit tests. Implemented correct mana cake behaviour. Created new output file to assert correct values/behaviour

--- src/GildedRose/GildedRose.cs
+++ src/GildedRose/GildedRose.cs
@@ -15,29 +15,39 @@ public class GildedRose
         {
             case "Aged Brie": 
                 IncreaseQuality(item, 1);
+                if (item.SellIn < 0)
+                {
+                    IncreaseQuality(item, 1);
+                }
                     break;
 
             case "Backstage passes to a TAFKAL80ETC concert":
-                if (item.SellIn > 0)
+                if (item.SellIn >= 0)
                 {
-                    if (item.SellIn <= 10 && item.SellIn > 5)
+                    IncreaseQuality(item, 1);
+
+                    if (item.SellIn < 10)
                     {
-                        IncreaseQuality(item, 2);
+                        IncreaseQuality(item, 1);
                     }
-                    else if (item.SellIn <= 5)
+                    if (item.SellIn < 5)
                     {
-                        IncreaseQuality(item, 3);
-                    }
-                    else
-                    {
-                        DecreaseQuality(item, 1);
+                        IncreaseQuality(item, 1);
                     }
                 }
-                else if (item.Quality > 0)
+                else if (item.Quality > 0 && item.SellIn < 0)
                 {
                     DecreaseQuality(item, item.Quality);
                 }
                     break;
+
+            case "Conjured Mana Cake" :
+                DecreaseQuality(item, 2);
+                if (item.SellIn < 0)
+                {
+                    DecreaseQuality(item, 2);
+                }
+                break;
 
             default:
                 DecreaseQuality(item, 1);
@@ -62,7 +72,13 @@ public class GildedRose
         if (item.Quality > 0)
         {
             item.Quality = item.Quality - amount;
+
         }
+    }
+
+    private void UpdateSellIn(Item item)
+    {
+        item.SellIn--;
     }
     
 
@@ -71,12 +87,19 @@ public class GildedRose
 
         foreach (var item in Items)
         {
+            if (item.Name == "Sulfuras, Hand of Ragnaros")
+            {
+                continue;
+            }
+
+            UpdateSellIn(item);
             UpdateItemQuality(item);
 
         }
 
         //for (var i = 0; i < Items.Count; i++)
         //{
+        //      ***NORMAL ITEMS***
         //    if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
         //    {
         //        if (Items[i].Quality > 0)
@@ -87,15 +110,16 @@ public class GildedRose
         //            }
         //        }
         //    }
-        //    else
-        //    {
-        //        if (Items[i].Quality < 50)
+        //    else 
+        //    {     ***"Aged Brie" AND BACKSTAGE PASS***
+        //        if (Items[i].Quality < 50) QUALITY NEVER EXCEEDS 50
         //        {
         //            Items[i].Quality = Items[i].Quality + 1;
-
+                       
+                       //BACKSTAGE PASS ADDITIONAL QUALITY INCREASES
         //            if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
         //            {
-        //                if (Items[i].SellIn < 11)
+        //                if (Items[i].SellIn < 11) 10 DAYS REMAINING GET ADDITIONAL +1
         //                {
         //                    if (Items[i].Quality < 50)
         //                    {
@@ -103,7 +127,7 @@ public class GildedRose
         //                    }
         //                }
 
-        //                if (Items[i].SellIn < 6)
+        //                if (Items[i].SellIn < 6) 5 DAYS REMAINING GET ADDITIONAL +1
         //                {
         //                    if (Items[i].Quality < 50)
         //                    {
@@ -119,6 +143,7 @@ public class GildedRose
         //        Items[i].SellIn = Items[i].SellIn - 1;
         //    }
 
+
         //    if (Items[i].SellIn < 0)
         //    {
         //        if (Items[i].Name != "Aged Brie")
@@ -129,12 +154,14 @@ public class GildedRose
         //                {
         //                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
         //                    {
+                                  //REDUCE NORMAL ITEM QUALITY TWICE AS FAST AS SELL IN IS LESS THAN 0 
         //                        Items[i].Quality = Items[i].Quality - 1;
         //                    }
         //                }
         //            }
         //            else
         //            {
+                           //DEDUCT QUALITY BY ITSELF AS QUALITY IS NEVER LOWER THAN 0
         //                Items[i].Quality = Items[i].Quality - Items[i].Quality;
         //            }
         //        }
@@ -142,6 +169,7 @@ public class GildedRose
         //        {
         //            if (Items[i].Quality < 50)
         //            {
+                            //INCREASE QUALITY IF IN DATE
         //                Items[i].Quality = Items[i].Quality + 1;
         //            }
         //        }
--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -7,18 +7,20 @@ public class GildedRoseTest
     private const string BackStagePass = "Backstage passes to a TAFKAL80ETC concert";
     private const string AgedBrie = "Aged Brie";
     private const string ElixirOfTheMongoose = "Elixir of the Mongoose";
+    private const string SulfurasHandOfRagnaros = "Sulfuras, Hand of Ragnaros";
+    private const string ConjuredManaCake = "Conjured Mana Cake";
 
     [Fact]
     public void Foo()
     {
-        List<Item> items = [ new Item { Name = "foo", SellIn = 0, Quality = 0 } ];
+        List<Item> items = [new Item { Name = "foo", SellIn = 0, Quality = 0 }];
         GildedRose app = new(items);
         app.UpdateItems();
         Assert.Equal("foo", items[0].Name);
     }
 
     [Fact]
-    public void IncreaseQuality_SellByDatePassed_SellInIsNegative() 
+    public void IncreaseQuality_SellByDatePassed_SellInIsNegative()
     {
         List<Item> items = [new Item { Name = ElixirOfTheMongoose, SellIn = -1, Quality = 10 }];
         GildedRose app = new(items);
@@ -29,10 +31,10 @@ public class GildedRoseTest
     [Fact]
     public void IncreaseQuality_SellByDatePassed_SellInIsZero()
     {
-        List<Item> items = [new Item { Name = ElixirOfTheMongoose, SellIn = 0, Quality = 10 }];
+        List<Item> items = [new Item { Name = ElixirOfTheMongoose, SellIn = -1, Quality = 10 }];
         GildedRose app = new(items);
         app.UpdateItems();
-        Assert.Equal(9, items[0].Quality);
+        Assert.Equal(8, items[0].Quality);
     }
 
     [Fact]
@@ -45,12 +47,20 @@ public class GildedRoseTest
     }
 
     [Fact]
+    public void IncreaseQuality_AgedBrieIncreaseQualityTwiceAsFast()
+    {
+        List<Item> items = [new Item { Name = AgedBrie, SellIn = -1, Quality = 10 }];
+        GildedRose app = new(items);
+        app.UpdateItems();
+        Assert.Equal(12, items[0].Quality);
+    }
+    [Fact]
     public void IncreaseQuality_BackstagePass_10DaysOrLess()
     {
         List<Item> items =
             [
-            new Item { Name = BackStagePass, SellIn = 10, Quality = 10 },
-            new Item { Name = BackStagePass, SellIn = 5, Quality = 5 },
+            new Item { Name = BackStagePass, SellIn = 9, Quality = 10 },
+            new Item { Name = BackStagePass, SellIn = 4, Quality = 5 },
             new Item { Name = BackStagePass, SellIn = -1, Quality = 5 }
             ];
 
@@ -59,5 +69,32 @@ public class GildedRoseTest
         Assert.Equal(12, items[0].Quality);
         Assert.Equal(8, items[1].Quality);
         Assert.Equal(0, items[2].Quality);
+    }
+
+    [Fact]
+    public void UpdateItems_Sulfuras_Unaffected()
+    {
+        List<Item> items =
+            [
+            new Item { Name = SulfurasHandOfRagnaros, SellIn = 100, Quality = 80 },
+            ];
+
+        GildedRose app = new(items);
+        app.UpdateItems();
+        Assert.Equal(80, items[0].Quality);
+    }
+
+    [Fact]
+    public void DecreaseQuality_ConjuredManaCakeDegradesTwiceAsFast()
+    {
+        List<Item> items =
+            [
+                new Item { Name = ConjuredManaCake, SellIn = 0, Quality = 80 },
+                new Item { Name = ConjuredManaCake, SellIn = 2, Quality = 80 },
+            ];
+        GildedRose app = new(items);
+        app.UpdateItems();
+        Assert.Equal(76, items[0].Quality);
+        Assert.Equal(78, items[1].Quality);
     }
 }
